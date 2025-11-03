a1e0510cf7b62020d6c64498944e5befa5673287
Aniket Bafna <aniketbafna@Anikets-MacBook-Air.local>
2025-11-03 15:06:43 +0530

    readme file updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Aniket",
+    author_email="aniketbafna1@gmail.com",
+    packages=find_packages()
+)
